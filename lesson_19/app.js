40d96c38dfbafd6280675e1c4ccaa6fad7d10293
Dmytro Bihun <89932822+DmitriyBihun@users.noreply.github.com>
2025-06-08 20:59:21 +0300

    Add files via upload

---
+++ lesson_19/app.js
@@ -0,0 +1,10 @@
+const themeSwitcher = document.querySelector("[data-theme-picker]");
+
+themeSwitcher.addEventListener("change", () => {
+    if (themeSwitcher.value === "☀️") {
+        document.documentElement.setAttribute("data-theme", "☀️");
+    }
+    if (themeSwitcher.value === "🌑") {
+        document.documentElement.setAttribute("data-theme", "🌑");
+    }
+});
